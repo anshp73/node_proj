06db21231f3869caa90012c84fcb326523e0705b
Ansh Parmar <93419387+anshp73@users.noreply.github.com>
2022-10-11 02:12:57 +0530

    Update App.js

--- node project get,post/View/src/App.js
+++ node project get,post/View/src/App.js
@@ -6,15 +6,15 @@ const dbUrl = "http://localhost:7000/book"
 
 const App = () => {
 
-  const [title,setTitle] = useState("")
-  const [author,setAuthor] = useState("")
-  const [price,setPrice] = useState(0)
+  const [sname,setsname] = useState("")
+  const [section,setsection] = useState("")
+  const [cgpa,setcgpa] = useState(0)
   
   const handleSubmit = () => {
     Axios.post(dbUrl, {
-      Title: title,
-      Author: author,
-      Price: price
+      Name : sname,
+      Section : section,
+      Cgpa: cgpa
     }).then( () => alert("Successfully logged!"))
   } 
 
@@ -23,11 +23,11 @@ const App = () => {
       <h1><b> CRUD Application </b></h1>
       <div className='details'>
         <label id='field'> Title of the Book </label>
-        <input type='text' name='title' onChange={ (e) => setTitle(e.target.value)} required/>
+        <input type='text' name='sname' onChange={ (e) => setsname(e.target.value)} required/>
         <label id='field'> Name of the Author </label>
-        <input type='text' name='author' onChange={ (e) => setAuthor(e.target.value)} required/>
+        <input type='text' name='section' onChange={ (e) => setsection(e.target.value)} required/>
         <label id='field'> Price </label>
-        <input type='number' name='price' onChange={ (e) => setPrice(e.target.value)} required />
+        <input type='number' name='cgpa' onChange={ (e) => setcgpa(e.target.value)} required />
         <button onClick={ handleSubmit }> SUBMIT </button>
       </div>
     </div>
